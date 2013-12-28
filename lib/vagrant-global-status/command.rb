b54a9ace08b84e14986577d66c4a61f483ab3979
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-28 12:07:20 +0900

    add feature to remove entries of non-created vms

--- lib/vagrant-global-status/command.rb
+++ lib/vagrant-global-status/command.rb
@@ -12,12 +12,15 @@ module VagrantPlugins
           o.on("-a", "--all", "Displays information about all machines (instead of just the active ones)") do |f|
             options[:all] = true
           end
+          o.on("-r", "--remove_terminated_vms", "Removes the entries of terminated VMs") do |f|
+            options[:remove_terminated_vms] = true
+          end
         end
 
         @argv = parse_options(opts)
         return if !@argv
 
-        registry = GlobalRegistry.new(@env.home_path.join('machine-environments.json'))
+        registry = GlobalRegistry.new(@env.home_path.join('machine-environments.json'), options)
         registry.environments.each do |env|
           status = "#{env.status(options[:all])}"
           if not status == ""
